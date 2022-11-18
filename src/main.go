9b1f117c8bfbec693d3c7e6c6dcf41c2874b30aa
Florian Boulbes <florian.boulbes@ynov.com>
2022-11-18 14:20:57 +0100

    fix: css file work

--- src/main.go
+++ src/main.go
@@ -7,8 +7,8 @@ import (
 
 func main() {
 	http.HandleFunc("/", home)
-	fs := http.FileServer(http.Dir("css"))
-	http.Handle("/css/", http.StripPrefix("templates/css/", fs))
+	fs := http.FileServer(http.Dir("templates"))
+	http.Handle("/css/", fs)
 	fmt.Println("(http://localhost:8080) - Server started on port", port)
 	http.ListenAndServe(port, nil)
 }
