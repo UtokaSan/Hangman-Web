0eebe2aea1be1a5ec6d16d8ebba44d55693b7c9d
Florian <74573533+UtokaSan@users.noreply.github.com>
2022-12-07 19:30:38 +0100

    update: optimize code

--- hangman-web/cmd/handlers.go
+++ hangman-web/cmd/handlers.go
@@ -15,16 +15,13 @@ type HangmanWeb struct {
 
 const port = ":8080"
 
-func Game(p *HangmanWeb) {
-	c := hangman_web.HangmanData{}
-	p.Word = hangman_web.Dictionnary("./words/words.txt")
-	p.Display = hangman_web.Display(p.Word, c.RandomLetter)
-}
-
 func Home(w http.ResponseWriter, r *http.Request) {
-	p := HangmanWeb{}
-	Game(&p)
-	renderTemplate(w, "./hangman-web/templates/home", p)
+	test := hangman_web.Dictionnary("./words/words.txt")
+	display := hangman_web.Display(test, "u")
+	fmt.Print(r.FormValue("inputText"))
+	renderTemplate(w, "./hangman-web/templates/home", HangmanWeb{
+		Word: display,
+	})
 }
 
 func renderTemplate(w http.ResponseWriter, tmpl string, p HangmanWeb) {
@@ -39,3 +36,5 @@ func renderTemplate(w http.ResponseWriter, tmpl string, p HangmanWeb) {
 // https://astaxie.gitbooks.io/build-web-application-with-golang/content/en/04.1.html
 // https://medium.com/@edwardpie/processing-form-request-data-in-golang-2dff4c2441be
 // https://www.youtube.com/watch?v=GnLHI_nekm8
+// https://golangcode.com/add-a-http-cookie/
+//https://youtu.be/ONAnstqcEcA
