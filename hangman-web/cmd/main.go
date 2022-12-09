f125d4696da121afc89581a0c9d846c03e0956a5
Florian Boulbes <florian.boulbes@ynov.com>
2022-12-09 17:08:57 +0100

    update: input hide for value

--- hangman-web/cmd/main.go
+++ hangman-web/cmd/main.go
@@ -9,8 +9,7 @@ func Runner() {
 	server := http.NewServeMux()
 	server.HandleFunc("/", Home)
 	fs := http.FileServer(http.Dir("static"))
-	http.Handle("./hangman-web/templates/static/stylesheets/", http.StripPrefix("./hangman-web/templates/static/stylesheets/", fs))
-
+	http.Handle("./hangman-web/templates/static/stylesheets/", http.StripPrefix("./hangman-web/templates/static/stylesheets/", fs))g
 	fmt.Println("http://localhost:8080")
 	err := http.ListenAndServe(port, server)
 	if err != nil {
