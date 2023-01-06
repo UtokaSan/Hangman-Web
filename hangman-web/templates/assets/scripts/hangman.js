7966e8d07ae9ea1ba1f1890031fa6c606f2847ab
Florian <florian.boulbes@ynov.com>
2023-01-06 19:40:03 +0100

    add: fetch post

--- hangman-web/templates/assets/scripts/hangman.js
+++ hangman-web/templates/assets/scripts/hangman.js
@@ -1,1 +1,19 @@
 console.log("JS Launched");
+
+const form = document.querySelector('form');
+
+form.addEventListener('submit', event => {
+    event.preventDefault();
+
+    const formData = new FormData(form);
+
+    fetch('http://localhost:8080/get', {
+        method: 'POST',
+        body: formData
+    })
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById("test").innerHTML = data;
+            console.log("")
+        });
+});
